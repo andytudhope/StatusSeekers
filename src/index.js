b63d704ecfcba770fc97e18ad9ad9ee450908738
andytudhope <tuddy0525@gmail.com>
2018-01-05 09:13:57 +0200

    Added localStorage for found words

--- src/index.js
+++ src/index.js
@@ -7,20 +7,29 @@ import thunkMiddleware from 'redux-thunk'
 import App from './App'
 import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker'
+import { loadState, saveState } from './localStorage'
 
 import './index.css'
 
 // Redux DevTools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistedState = loadState();
 const store = createStore(
   reducer,
+  persistedState,
   composeEnhancers(
     applyMiddleware(
       thunkMiddleware
     )
   )
 )
+
+// Save the state to localStorage any time it changes, using throttle to make
+// sure this doesn't happen too often, given the use of JSON.stringify()
+store.subscribe(() => {
+  saveState(store.getState());
+})
 
 ReactDOM.render(
   <Provider store={store}>
